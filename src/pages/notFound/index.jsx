c5ff2671b7b30735510fad8759354314b4f0f45f
JuliaMukha <Julia-Mukha1986@yandex.by>
2026-08-01 00:07:02 +0300

    fix: add NotFound page with styles and fix router issues

--- src/pages/notFound/index.jsx
+++ src/pages/notFound/index.jsx
@@ -1,5 +1,17 @@
-const NotFound = () => {
-    return <></>
-}
+import { Link } from "react-router";
+import "./style.scss";
 
-export default NotFound
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+            <p>Sorry, the page you are looking for does not exist.</p>
+            <Link to="/" className="not-found__link">
+                Go to Home
+            </Link>
+        </div>
+    );
+};
+
+export default NotFound;
